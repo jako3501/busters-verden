6331dddbde4a420a27998885c3776d65e3b6bf09
jako3501 <jako3501@easv365.dk>
2023-09-13 13:53:22 +0200

    bootstrap fix

--- functions.php
+++ functions.php
@@ -9,7 +9,11 @@ add_action( "init", "ad_remove_gutenberg");
 
 function buster_get_stylesheet() {
     wp_enqueue_style('style', get_stylesheet_uri('style.css'));
-    wp_enqueue_style('bootstrap', get_template_directory_uri('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'));
+    wp_enqueue_style( 'bootstrap-cdn-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
+    wp_enqueue_script( 'bootstrap-cdn-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' );
+    // ------               -------//
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
     wp_enqueue_script('script', get_template_directory_uri('script.js'));
 }
 
