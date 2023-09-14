44f2e604eb2bc66daf90f2a84f9f9b72100f6db8
jako3501 <jako3501@easv365.dk>
2023-09-14 15:22:05 +0200

    bootstrap issue

--- footer.php
+++ footer.php
@@ -1,4 +1,43 @@
 <?php wp_footer(); ?>
-</body>
-</html>
 
+<footer>
+    <div class="footer-container">
+        <!-- Facebook -->
+        <div class="facebook-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256"
+                width="48px" height="48px">
+                <g fill="#fe8e22" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                    stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                    font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                    style="mix-blend-mode: normal">
+                    <g transform="scale(10.66667,10.66667)">
+                        <path
+                            d="M21,3h-18v18h9.621v-6.961h-2.343v-2.725h2.343v-2.005c0,-2.324 1.421,-3.591 3.495,-3.591c0.699,-0.002 1.397,0.034 2.092,0.105v2.43h-1.428c-1.13,0 -1.35,0.534 -1.35,1.322v1.735h2.7l-0.351,2.725h-2.365v6.965h5.586z">
+                        </path>
+                    </g>
+                </g>
+            </svg>
+        </div>
+
+        <!-- Instagram -->
+        <div class="instagram-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256"
+                width="48px" height="48px">
+                <g fill="#fe8e22" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                    stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                    font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                    style="mix-blend-mode: normal">
+                    <g transform="scale(10.66667,10.66667)">
+                        <path
+                            d="M3,3v18h18v-18zM18,5c0.552,0 1,0.448 1,1c0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c0,-0.552 0.448,-1 1,-1zM12,7c2.761,0 5,2.239 5,5c0,2.761 -2.239,5 -5,5c-2.761,0 -5,-2.239 -5,-5c0,-2.761 2.239,-5 5,-5zM12,9c-1.65685,0 -3,1.34315 -3,3c0,1.65685 1.34315,3 3,3c1.65685,0 3,-1.34315 3,-3c0,-1.65685 -1.34315,-3 -3,-3z">
+                        </path>
+                    </g>
+                </g>
+            </svg>
+        </div>
+    </div>
+</footer>
+
+</body>
+
+</html>
