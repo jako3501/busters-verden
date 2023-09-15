8ed2cea6c21df48c61b81f425ff6824ef02c1e02
jako3501 <jako3501@easv365.dk>
2023-09-15 18:03:17 +0200

    page.php leggo

--- page.php
+++ page.php
@@ -1,122 +1,132 @@
 <?php get_header() ?>
 
-<div class="hero-container">
-    <div class="headline">
-        <h1>PROGRAM</h1>
-    </div>
-    <img src="http://wp-template2.local/wp-content/uploads/2023/09/program-heroimg.png" alt="">
+<?php get_fields('page_title'); ?>
 
-</div>
+<?php while (have_posts()):
+    the_post() ?>
 
-<div class="container2">
-    <h2>
-        <?php the_field('program-intro') ?>
-    </h2>
-</div>
+    <?php the_content() ?>
 
-<div class="pa-scenen">
-    <div class="skuespillere-box">
-        <h3>PÅ SCENEN</h3>
-        <?php
-        $loop = new WP_Query(
-            array(
-                "post_type" => "skuespiller",
-                "posts_per_page" => -1
-            )
-        );
-        ?>
-        <?php while ($loop->have_posts()):
-            $loop->the_post() ?>
-            <h4>
-                <?php the_title() ?>
-            </h4>
-            <p>
-                <?php the_field("navn") ?>
-            </p>
-            <p>
-                <?php the_field("navn2") ?>
-            </p>
-            <p>
-                <?php the_field("navn3") ?>
-            </p>
-            <p>
-                <?php the_field("navn4") ?>
-            </p>
-            <p>
-                <?php the_field("navn5") ?>
-            </p>
-            <p>
-                <?php the_field("navn6") ?>
-            </p>
-            <p>
-                <?php the_field("navn7") ?>
-            </p>
-            <p>
-                <?php the_field("navn8") ?>
-            </p>
-            <p>
-                <?php the_field("navn9") ?>
-            </p>
-            <p>
-                <?php the_field("navn10") ?>
-            </p>
 
-        <?php endwhile; ?>
-        <?php wp_reset_postdata() ?>
-    </div>
-    <div class="medvirkende-box">
-        <h3>PRAKTISKE</h3>
-        <?php
-        $loop = new WP_Query(
-            array(
-                "post_type" => "medvirke",
-                "posts_per_page" => -1
-            )
-        );
-        ?>
-        <?php while ($loop->have_posts()):
-            $loop->the_post() ?>
-            <h4>
-                <?php the_title() ?>
-            </h4>
-            <p>
-                <?php the_field("navn") ?>
-            </p>
-            <p>
-                <?php the_field("navn2") ?>
-            </p>
-            <p>
-                <?php the_field("navn3") ?>
-            </p>
-            <p>
-                <?php the_field("navn4") ?>
-            </p>
-            <p>
-                <?php the_field("navn5") ?>
-            </p>
-            <p>
-                <?php the_field("navn6") ?>
-            </p>
-            <p>
-                <?php the_field("navn7") ?>
-            </p>
-            <p>
-                <?php the_field("navn8") ?>
-            </p>
-            <p>
-                <?php the_field("navn9") ?>
-            </p>
-            <p>
-                <?php the_field("navn10") ?>
-            </p>
 
-        <?php endwhile; ?>
-        <?php wp_reset_postdata() ?>
+
+    <div class="hero-container">
+        <div class="headline">
+            <h1>PROGRAM</h1>
+        </div>
+        <img src="http://wp-template2.local/wp-content/uploads/2023/09/program-heroimg.png" alt="">
 
     </div>
-</div>
+
+    <div class="container2">
+        <h2>
+            <?php the_field('program-intro') ?>
+        </h2>
+    </div>
+
+    <div class="pa-scenen">
+        <div class="skuespillere-box">
+            <h3>PÅ SCENEN</h3>
+            <?php
+            $loop = new WP_Query(
+                array(
+                    "post_type" => "skuespiller",
+                    "posts_per_page" => -1
+                )
+            );
+            ?>
+            <?php while ($loop->have_posts()):
+                $loop->the_post() ?>
+                <h4>
+                    <?php the_title() ?>
+                </h4>
+                <p>
+                    <?php the_field("navn") ?>
+                </p>
+                <p>
+                    <?php the_field("navn2") ?>
+                </p>
+                <p>
+                    <?php the_field("navn3") ?>
+                </p>
+                <p>
+                    <?php the_field("navn4") ?>
+                </p>
+                <p>
+                    <?php the_field("navn5") ?>
+                </p>
+                <p>
+                    <?php the_field("navn6") ?>
+                </p>
+                <p>
+                    <?php the_field("navn7") ?>
+                </p>
+                <p>
+                    <?php the_field("navn8") ?>
+                </p>
+                <p>
+                    <?php the_field("navn9") ?>
+                </p>
+                <p>
+                    <?php the_field("navn10") ?>
+                </p>
+
+            <?php endwhile; ?>
+            <?php wp_reset_postdata() ?>
+        </div>
+        <div class="medvirkende-box">
+            <h3>PRAKTISKE</h3>
+            <?php
+            $loop = new WP_Query(
+                array(
+                    "post_type" => "medvirke",
+                    "posts_per_page" => -1
+                )
+            );
+            ?>
+            <?php while ($loop->have_posts()):
+                $loop->the_post() ?>
+                <h4>
+                    <?php the_title() ?>
+                </h4>
+                <p>
+                    <?php the_field("navn") ?>
+                </p>
+                <p>
+                    <?php the_field("navn2") ?>
+                </p>
+                <p>
+                    <?php the_field("navn3") ?>
+                </p>
+                <p>
+                    <?php the_field("navn4") ?>
+                </p>
+                <p>
+                    <?php the_field("navn5") ?>
+                </p>
+                <p>
+                    <?php the_field("navn6") ?>
+                </p>
+                <p>
+                    <?php the_field("navn7") ?>
+                </p>
+                <p>
+                    <?php the_field("navn8") ?>
+                </p>
+                <p>
+                    <?php the_field("navn9") ?>
+                </p>
+                <p>
+                    <?php the_field("navn10") ?>
+                </p>
+
+            <?php endwhile; ?>
+            <?php wp_reset_postdata() ?>
+
+        </div>
+    </div>
 
 
-
+<?php endwhile; ?>
 
 <?php get_footer() ?>
