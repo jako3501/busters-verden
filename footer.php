cdcbda368c3cd65ded9940ba08e18cbef1e7c76f
Kajmak101 <Adnan_s@live.dk>
2023-09-17 19:57:44 +0200

    semifixed reson

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 
     <div class="some">
         <a href="<?php the_field('link-facebook') ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icons8-facebook-48 (1).png " alt=""></a>
-        <a href="<?php the_field('link-facebook') ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icons8-instagram-48.png " alt=""></a>
+        <a href="<?php the_field('link-instagram') ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icons8-instagram-48.png " alt=""></a>
     </div>
 
     <p>Kirkegade 51, 6700 Esbjerg</p>
